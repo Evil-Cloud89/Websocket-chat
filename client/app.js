db798a32152b6588edc781964fe3eb13839fbf99
Mateusz Oko <mo89mo@gmail.com>
2023-06-26 15:33:32 +0200

    Add login form submission and validation

--- client/app.js
+++ client/app.js
@@ -6,3 +6,16 @@ const messagesList = document.getElementById('messages-list');
 const addMessageForm = document.getElementById('add-messages-form');
 const userNameInput = document.getElementById('username');
 const messageContentInput = document.getElementById('message-content');
+
+loginForm.addEventListener('submit', login);
+
+function login(event) {
+  event.preventDefault();
+  if(userNameInput.value === '') {
+    alert('Name field cannot be empty!');
+    return;
+  }
+  userName = userNameInput.value;
+  loginForm.classList.remove('show');
+  messagesSection.classList.add('show');
+}
